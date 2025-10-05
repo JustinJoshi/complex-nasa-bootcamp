4e8bcceb0b8ad0323ef7a5ac4ed5fcab6a15d577
Justin Joshi <justinjoshi@proton.me>
2025-10-04 22:27:50 -0400

    first commit

--- README.md
+++ README.md
@@ -20,3 +20,19 @@ I completed the challenge: 5
 I feel good about my code: 4
 I'm not sure if my constructors are setup cleanly...
 ```
+
+Nasa into weather API
+
+This api grabs all the nasa facilities and their location, and feeds that info into a weather api.
+
+Tech used: HTML, CSS, JavaScript, Framework of choice
+
+i used a proxy url site to grab the info from the nasa api to avoid the cors request issue. i used a fetch request to fetch the data, then fed that data into another fetch request to then print all the required data into the dom with innerhtml
+
+Optimizations
+
+make it prettier maybe
+
+Lessons Learned:
+
+i learned that proxy urls can work but this one was also very slow. I learned how to use the async await syntax properly but still opted not to use it in this case.
--- js/main.js
+++ js/main.js
@@ -2,44 +2,46 @@
 
 //the main thing that i had trouble with was getting the cors proxy to work. It wasnt working for me for a long time, but it ended up starting to work so now im using it
 
+//the proxy stopped working again :D so im using a big object now
+
 const nasaUrl = `https://corsproxy.io/?url=https://data.nasa.gov/docs/legacy/gvk9-iz74.json`
 
 const facilities = document.querySelector('#facilities')
 const loc = document.querySelector('#location')
 
-//i did have trouble using async syntax and for loops, so i applied hector's strategy of using the basic fetch syntax
+// i did have trouble using async syntax and for loops, so i applied hector's strategy of using the basic fetch syntax
 
 fetch(nasaUrl)
     .then(res => res.json()) // parse response as JSON
     .then(nasaData => {
         nasaData.forEach((x, i) => {
-
             const lat = nasaData[i].location.latitude
             const lon = nasaData[i].location.longitude
-
-
-
-            const weatherUrl = `http://api.openweathermap.org/data/2.5/weather?q=${lat},${lon}&APPID=0e904a2a7aacc8772e00052c29bf80c8`;
+            const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=0e904a2a7aacc8772e00052c29bf80c8`;
             fetch(weatherUrl)
                 .then(res => res.json()) // parse response as JSON
                 .then(weatherData => {
 
-                    facilities.innerHTML += `<div> ${nasaData[i].facility} || ${lat} ${lon} || ${weatherData}</div>`
+                    facilities.innerHTML += `<div> ${nasaData[i].facility} || ${lat} ${lon} || Temp: ${(((weatherData.main.temp - 273.15) * 1.8) + 32).toFixed(1)} F | Humidity: ${weatherData.main.humidity} | Sky: ${weatherData.weather[0].description}</div>`
 
                 })
                 .catch(err => {
                     console.log(`error ${err}`)
                 });
-
-
-
-
-
         })
-        console.log(nasaData[0])
-
     })
     .catch(err => {
         console.log(`error ${err}`)
     });
 
+// const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${37.911289}&lon=${-75.469622}&APPID=0e904a2a7aacc8772e00052c29bf80c8`;
+// fetch(weatherUrl)
+//     .then(res => res.json()) // parse response as JSON
+//     .then(weatherData => {
+
+//         facilities.innerHTML = `Temp: ${(((weatherData.main.temp - 273.15) * 1.8) + 32).toFixed(1)} F | Humidity: ${weatherData.main.humidity} | Sky: ${weatherData.weather[0].description}`
+
+//     })
+//     .catch(err => {
+//         console.log(`error ${err}`)
+//     });
